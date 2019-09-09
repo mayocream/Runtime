19c02d8cad0a9d20fd1113ed307511817f003be4
Mayo <mayocream39@yahoo.co.jp>
2019-09-09 22:31:37 +0800

    fix ab

--- lsyncd/docker-entrypoint.sh
+++ lsyncd/docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 chmod 600 /etc/rsync/rsync.password /etc/rsync/rsync.secrets
 
+rm -rf /var/run/rsyncd.pid /var/run/lsyncd.pid
+
 rsync --daemon --config=/etc/rsync/rsync.conf
 lsyncd /etc/lsyncd/lsyncd.conf
 
